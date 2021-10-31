06f6be8254eb35abee6b846b0af57612a1614256
Biswa-bob <biswajeetpanda.bp@gmail.com>
2021-10-31 21:15:46 +0530

    added teammates id

--- src/components/Team.js
+++ src/components/Team.js
@@ -11,25 +11,25 @@ const Team = (props) => {
             src="assets/img/abhisek.jpeg"
             title="Abhisek Yadav"
             post="Co-founder & Blockchain Developer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/abhisekyadav/"
+            github="https://github.com/Abhisek0x"
+            insta="https://www.instagram.com/_abhisek_yadav/"
           />
           <TeamCard
             src="assets/img/sifan.jpeg"
             title="Sifan Khan"
             post="Co-founder & Blockchain Developer"
-            linkedin="#"
-            github="#"
+            linkedin="https://www.linkedin.com/in/sifan-khan-2359a1192/"
+            github="https://github.com/Zephyr-Phoenix"
             insta="#"
           />
           <TeamCard
             src="assets/img/mypic.jpg"
             title="Prateek Nair"
             post="Co-founder & Web Developer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/prateek-nair-91489719a/"
+            github="https://github.com/prateek-jpg"
+            insta="https://www.instagram.com/prat___07/"
           />
         </div>
         <div className="cont">
@@ -37,33 +37,33 @@ const Team = (props) => {
             src="assets/img/biswajeet.jpeg"
             title="Biswajeet Panda"
             post="Co-founder & Web Developer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/biswajeetpanda-/"
+            github="https://github.com/Biswa-bob"
+            insta="https://www.instagram.com/b.o.b_builder_/"
           />
           <TeamCard
             src="assets/img/pratyush.jpeg"
             title="Pratyush Vakadi"
             post="Unity Developer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/vakadi-sai-pratyush-ba9b18200/"
+            github="https://github.com/Sai-Pratyush"
+            insta="https://www.instagram.com/pratyushvakadi5711/"
           />
           <TeamCard
             src="assets/img/ishita.jpeg"
             title="Ishita Satpathy"
             post="Unity Developer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/ishita-satpathy-619a38201/"
+            github="https://github.com/IshitaSatpathy"
+            insta="https://www.instagram.com/ishitasatpathy/"
           />
           <TeamCard
             src="assets/img/ritu.jpg"
             title="Ritu Mohapatro"
             post="UI/UX Designer"
-            linkedin="#"
-            github="#"
-            insta="#"
+            linkedin="https://www.linkedin.com/in/ritu-rani-mohapatra-45a0bb209/"
+            github="https://github.com/RituRanii"
+            insta="https://www.instagram.com/__a_m_b_i_v_e_r_t.__/"
           />
         </div>
       </section>
