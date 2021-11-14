81c8a037d2491de99c1e72c4808b5ef1e938620c
cosmoinfinitas <91360858+cosmoinfinitas@users.noreply.github.com>
2021-11-14 12:52:22 +0530

    added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 const Contact = () => {
   const [data, setData] = useState({
     name: "",
-    subject: "",
     message: "",
     email: "",
   });
@@ -19,11 +18,42 @@ const Contact = () => {
     });
   };
 
-  const formSubmit = (e) => {
+  const formSubmit = async (e) => {
     e.preventDefault();
-    alert(
-      `Hello, ${data.name}.Your Email Id is ${data.email}.The Subject is ${data.subject} and your feedback is ${data.message}`
-    );
+    // alert(
+    //   `Hello, ${data.name}.Your Email Id is ${data.email}.The Subject is ${data.subject} and your feedback is ${data.message}`
+    // );
+    const { name, email, message } = data;
+
+    if (name && email && message) {
+      const res = fetch(
+        "https://cosmoinfinitas-00-default-rtdb.firebaseio.com/userDataRecords.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            message,
+          }),
+        }
+      );
+
+      if (res) {
+        setData({
+          name: "",
+          email: "",
+          message: "",
+        });
+        alert("Message Received Successfully !");
+      } else {
+        alert("plz fill the data");
+      }
+    } else {
+      alert("plz fill the data");
+    }
   };
   const style = {
     border: "0",
@@ -37,122 +67,6 @@ const Contact = () => {
   return (
     <>
       <section id="contact" class="contact">
-        {/* <div class="container" data-aos="fade-up">
-          <div class="section-title">
-            <h2>Contact</h2>
-            <p style={s}>Contact Us</p>
-          </div>
-
-          <div class="row" >
-            <div class="col-lg-5 d-flex align-items-stretch">
-              <div class="info">
-                <div class="address">
-                  <i class="icofont-reddit"></i>
-                  <h4>Join Our Discord</h4>
-                  <br />
-                </div>
-
-                <div class="email">
-                  <i class="icofont-envelope"></i>
-                  <h4>Email:</h4>
-                  <p>info@example.com</p>
-                </div>
-
-                <div class="phone">
-                  <i class="icofont-phone"></i>
-                  <h4>Call:</h4>
-                  <p>+1 5589 55488 55s</p>
-                </div>
-                <br />
-
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3712.2708389932204!2d83.90192878329283!3d21.49710889006581!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a213da4ec28af2d%3A0xb33a5cc9af8c33c7!2sVeer%20Surendra%20Sai%20University%20of%20Technology!5e0!3m2!1sen!2sin!4v1629637740810!5m2!1sen!2sin"
-                  frameborder="0"
-                  style={style}
-                  allowfullscreen
-                ></iframe>
-              </div>
-            </div>
-
-            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
-              <form
-                action="forms/contact.php"
-                method="post"
-                role="form"
-                class="php-email-form"
-                onSubmit={formSubmit}
-              >
-                <div class="form-row">
-                  <div class="form-group col-md-12">
-                    <label for="name">Your Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      class="form-control"
-                      id="name"
-                      value={data.name}
-                      onChange={InputEvent}
-                      data-rule="minlen:4"
-                      data-msg="Please enter at least 4 chars"
-                    />
-                    <div class="validate"></div>
-                  </div>
-                  <div class="form-group col-md-12">
-                    <label for="name">Your Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      name="email"
-                      value={data.email}
-                      id="email"
-                      onChange={InputEvent}
-                      data-rule="email"
-                      data-msg="Please enter a valid email"
-                    />
-                    <div class="validate"></div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="name">Subject</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    name="subject"
-                    value={data.subject}
-                    onChange={InputEvent}
-                    id="subject"
-                    data-rule="minlen:4"
-                    data-msg="Please enter at least 8 chars of subject"
-                  />
-                  <div class="validate"></div>
-                </div>
-                <div class="form-group">
-                  <label for="name">Message</label>
-                  <textarea
-                    class="form-control"
-                    name="message"
-                    value={data.message}
-                    onChange={InputEvent}
-                    rows="12"
-                    data-rule="required"
-                    data-msg="Please write something for us"
-                  ></textarea>
-                  <div class="validate"></div>
-                </div>
-                <div class="mb-3">
-                  <div class="loading">Loading</div>
-                  <div class="error-message"></div>
-                  <div class="sent-message">
-                    Your message has been sent. Thank you!
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button type="submit">Send Message</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div> */}
         <div class="section-title">
           <p style={s}>Contact Us</p>
         </div>
@@ -180,18 +94,45 @@ const Contact = () => {
             <div class="right-side">
               <div class="topic-text">Send us a message</div>
               {/* <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p> */}
-              <form action="#">
+              <form method="POST" id="contactform">
                 <div class="input-box">
-                  <input type="text" placeholder="Enter your name" />
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Enter your name"
+                    value={data.name}
+                    onChange={InputEvent}
+                    required
+                  />
                 </div>
                 <div class="input-box">
-                  <input type="text" placeholder="Enter your email" />
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    value={data.email}
+                    onChange={InputEvent}
+                    required
+                  />
                 </div>
                 <div class="input-box message-box">
-                  <input type="text" placeholder="Enter a short description" />
+                  <input
+                    type="text"
+                    id="message"
+                    name="message"
+                    placeholder="Enter a short description"
+                    value={data.message}
+                    onChange={InputEvent}
+                    required
+                  />
                 </div>
                 <div class="button">
-                  <input type="button" value="Send Now" />
+                  {/* <input type="button" value="Send" /> */}
+                  <button type="submit" onClick={formSubmit}>
+                    Send
+                  </button>
                 </div>
               </form>
             </div>
