4b3c3e6bf59c2d6e19ddf6493469c731b202dddc
Abhisek1999 <56746981+Abhisek1999@users.noreply.github.com>
2022-01-09 18:44:57 +0530

    added game link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,38 @@
 import React from "react";
 
 const Header = () => {
-  const market = "https://market.cosmoinfinitas.tech/";
-  const discord = "https://discord.gg/9YnyyBVjHA";
-  return (
-    <>
-      <section id="hero" class="d-flex align-items-center">
-        <canvas id="inner_heading-canvas" width="100" height="100" />
-        <div
-          class="container position-relative text-center text-lg-start"
-          data-aos="zoom-in"
-          data-aos-delay="100"
-        >
-          <div class="row">
-            <div class="col-lg-8">
-              <h1>
-                Welcome to <br /> <span>CosmoInfinitas</span>
-              </h1>
-              <h2>The World Of Infinite Possibilities</h2>
+	const market = "https://play.unity.com/mg/other/project-vsx-maze1";
+	const discord = "https://discord.gg/9YnyyBVjHA";
+	return (
+		<>
+			<section id="hero" class="d-flex align-items-center">
+				<canvas id="inner_heading-canvas" width="100" height="100" />
+				<div
+					class="container position-relative text-center text-lg-start"
+					data-aos="zoom-in"
+					data-aos-delay="100"
+				>
+					<div class="row">
+						<div class="col-lg-8">
+							<h1>
+								Welcome to <br /> <span>CosmoInfinitas</span>
+							</h1>
+							<h2>The World Of Infinite Possibilities</h2>
 
-              <div class="btns">
-                <a href={market} class="btn-menu animated fadeInUp scrollto">
-                  Start Exploring
-                </a>
-                <a href={discord} class="btn-book animated fadeInUp scrollto">
-                  Join Our Discord
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+							<div class="btns">
+								<a href={market} class="btn-menu animated fadeInUp scrollto">
+									Start Exploring
+								</a>
+								<a href={discord} class="btn-book animated fadeInUp scrollto">
+									Join Our Discord
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Header;
